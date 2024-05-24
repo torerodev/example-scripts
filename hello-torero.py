2c1141824048dd84f715b2ecb1f0b76c71660a21
Wyatt Sullivan <wyatts@gmail.com>
2024-05-24 09:56:22 -0500

    new hello, world script

---
+++ hello-torero.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def main():
+    print("Hello, torero!")
+    print("For a more complete example on how to leverage Python in torero, visit:")
+    print("https://docs.torero.dev/en/latest/python/")
+
+if __name__ == "__main__":
+    main()
+
