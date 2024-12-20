05cc33dcca1e8bbe57da03c6d8bf8c15df509045
ben-butera <ben.butera@gmail.com>
2024-12-20 14:13:58 -0500

    Create hello-you.py
    This is a hello world example that is designed for use with a decorator

---
+++ hello-you.py
@@ -0,0 +1,15 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--your_name')
+    args = parser.parse_args()
+
+    operator_name = args.your_name
+    
+    print("Hello ", operator_name, "!")
+    print("For more information on Itential Automation Service, check out:")
+    print("https://docs.itential.com/itential-cloud/docs/announcements")
+
+if __name__ == "__main__":
+    main()
